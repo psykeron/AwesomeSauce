59852b8e94cd50eae8c55ba317211163d05a6201
Colin Morris <colin@cs.toronto.edu>
2011-12-20 14:47:00 -0500

    Fixed intelligent_partition bug

--- code/intelligent_partition.py
+++ code/intelligent_partition.py
@@ -4,22 +4,33 @@
 from optparse import OptionParser
 import sys
 from collections import defaultdict
+from get_data_set import FILE_TYPES
+import random
 
-def build_offset_list(filename):
-    """This fuction will return a list of line offsets for the given file with name 'filename'.  
-    Once one has the list, it can be used to seek to a particular line in the file where in the index of the list is the line one
-    wishes to reference
-    """
-    the_file = open(filename)
-    line_offset = []
-    offset = 0
-    for line in the_file:
-	line_offset.append(offset)
-	offset += len(line)
-	
-    the_file.close()
+LABEL_LOOKUP = dict([ (num, ext) for (ext, num) in zip(FILE_TYPES, range(1,len(FILE_TYPES)+1))])
+
+# This quantifies how high we want to set the minimum fragments per class. A higher value will increase the minimum, but cause the actual ratio of training to testing data to be off. 
+# E.g. If there are 1000 fragments spread over 10 classes, and our ratio is 1.0, we will want a total of 100 fragments for
+# our test set. If DISPARITY_INTOLERANCE is set to 1.0, then each class will require a minimum of 10 fragments. Because
+# we either take all the fragments in a file or none at all, the actual number of test fragments will certainly exceed 100.
+# If DISPARITY_INTOLERANCE is set to 0.5, then each class will only need a minimum of 5 fragments.
+
+DISPARITY_INTOLERANCE = 0.6
+
+CEILING = 3.0
+
+class Fiel(object):
     
-    return line_offset
+    def __init__(self, fid, label):
+        self.fid = fid
+        self.label = label
+        
+    def __hash__(self):
+        # We can assume that fids are unique; that is, there are no two files with the same fid and different labels
+        return hash(self.fid)
+        
+    def __eq__(self, other):
+        return isinstance(other, Fiel) and self.fid == other.fid
 
   
 
@@ -43,33 +54,78 @@ if __name__ == '__main__':
     
     f = open(fname, 'r')
     
-    # Mapping of file id to vectors
+    # Mapping of Fiel objects to number of fragments
     id_to_nfrags = defaultdict(int)
 
     for line in f:
-        frag_id = line.split('#')[1].strip()
-        id_to_nfrags[frag_id] += 1
-    
-    #remember to close that shit!	
-    #f.close()
+        label = line.split()[0]
+        file_id = line.split('#')[1].strip()
+        this_fiel = Fiel(file_id, label)
+        id_to_nfrags[this_fiel] += 1
     
     total_frags = sum(id_to_nfrags.values() )
     ntest_frags = int(options.test_data_ratio*total_frags)
+    ntest_frags_orig = ntest_frags
+                                                          
+    
+    nclasses = len(FILE_TYPES)
+    min_frags_per_class = int(ntest_frags/(nclasses+0.0) * DISPARITY_INTOLERANCE)
+    # A dict mapping classes to the number of fragments required for that class
+    class_to_reqfrags = dict( (str(classlabel), min_frags_per_class) for classlabel in range(1, len(FILE_TYPES)+1) )
+    
+    print "Found %d total fragments over %d files. Aiming to use %d fragments for test data, with minimum %d per class" % (total_frags, len(id_to_nfrags), ntest_frags, min_frags_per_class)
     
     test_ids = set([])
-    for id_ in id_to_nfrags:
-        test_ids.add(id_)
-        ntest_frags -= id_to_nfrags[id_]
+    # Step one: attain the minimum for each class
+    for fiel in id_to_nfrags:
+        if class_to_reqfrags[fiel.label] > 0:
+            if id_to_nfrags[fiel] + (min_frags_per_class-class_to_reqfrags[fiel.label]) > (min_frags_per_class*CEILING):
+                print "Skipping file to avoid going above ceiling"
+            else:
+                test_ids.add(fiel.fid)
+                class_to_reqfrags[fiel.label] -= id_to_nfrags[fiel]
+                ntest_frags -= id_to_nfrags[fiel]
+            
+    print "After attaining minimum per each class with %d files, we have %d fragments left to fill" % (len(test_ids), ntest_frags)
+        
+    bugged = False
+    for label in range(1, len(FILE_TYPES)+1):
+        if class_to_reqfrags[str(label)] > 0 and label != 16:
+            print "Class %s still needs %d more fragments allocated" % (LABEL_LOOKUP[label], class_to_reqfrags[str(label)])
+            bugged = True
+    if bugged:
+        raise Exception("I done wrong.")
+    #assert not any([ (class_to_reqfrags[str(label)] > 0) for label in range(1, len(FILE_TYPES)+1) ]), "There were some file types that didn't get the minimum number of fragments, for some reason"
+    
+    fiels = id_to_nfrags.keys()
+    fiels = set(fiels)
+    fiels.difference_update(test_ids) # Have to make sure we don't double-add files
+    fiels = list(fiels)
+    random.shuffle(fiels)
+    
+    for fiel in fiels:
         if ntest_frags <= 0:
             break
+        if id_to_nfrags[fiel] + (min_frags_per_class-class_to_reqfrags[fiel.label]) > (min_frags_per_class*CEILING):
+            print "Skipping file to avoid going above ceiling"
+        else:
+            test_ids.add(fiel.fid)
+            ntest_frags -= id_to_nfrags[fiel]
+            class_to_reqfrags[fiel.label] -= id_to_nfrags[fiel]
+    else:
+        print "WARNING: exhausted file list before getting the required number of test fragments"
             
     print "Picked %d files to use in test file" % (len(test_ids))
+    actual_test_frags = ntest_frags_orig - ntest_frags
+    actual_ratio = actual_test_frags / (total_frags + 0.0)
+    print "Selected %d test fragments leading to an actual test:train ratio of %.2f" % (actual_test_frags, actual_ratio)
     
     test_f = open(options.test_fname, 'w')
     train_f = open(options.train_fname, 'w')
     
     f.seek(0)
     for line in f:
+        #label = line.split()[0]
         frag_id = line.split('#')[1].strip()
         if frag_id in test_ids:
             test_f.write(line)
